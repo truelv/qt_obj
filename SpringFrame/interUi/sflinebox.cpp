cd1a82bb703054a4624499f4db1d004570b370b6
zhuhuihui <1340044380@qq.com>
2022-03-04 10:34:38 +0800

    完成消息提示框子控件

--- SpringFrame/interUi/sflinebox.cpp
+++ SpringFrame/interUi/sflinebox.cpp
@@ -1,7 +1,25 @@
 #include "sflinebox.h"
+#include "ui_messagelinebox.h"
 
-SFLineBox::SFLineBox()
+SFLineBox::SFLineBox() :
+    ui(new Ui::MessageLineBox)
 {
 
+}
+
+SFLineBox::~SFLineBox()
+{
+    delete ui;
+}
+
+void SFLineBox::bindUi(QWidget *w)
+{
+    ui->setupUi(w);
+}
+
+void SFLineBox::updateCount(int num)
+{
+    printf("更新数字 %d\n", num);
+    ui->count->setText(QString::number(num));
 }
 
--- SpringFrame/interUi/sflinebox.h
+++ SpringFrame/interUi/sflinebox.h
@@ -1,11 +1,21 @@
 #ifndef SFLINEBOX_H
 #define SFLINEBOX_H
+#include "interui.h"
+#include <QWidget>
 
+namespace Ui {
+class MessageLineBox;
+}
 
 class SFLineBox : public InterUi
 {
 public:
     SFLineBox();
+    ~SFLineBox();
+    void bindUi(QWidget* w);
+    void updateCount(int num);
+
+    Ui::MessageLineBox *ui;
 };
 
 #endif // SFLINEBOX_H
