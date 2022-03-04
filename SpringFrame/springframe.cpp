8c20728858c2f9878ace9eef3b1be73174251915
zhuhuihui <1340044380@qq.com>
2022-03-04 10:45:09 +0800

    添加实现子控件的切换接口，添加创建子控件的接口，修改点击组件的确定/取消按键，都不销毁组件

--- SpringFrame/springframe.cpp
+++ SpringFrame/springframe.cpp
@@ -11,34 +11,25 @@ SpringFrame::SpringFrame(QWidget *parent, SF_TYPE type, const QRect &rect, bool 
 {
     // 主窗口
     ui->setupUi(this);
-    setWindowFlags(Qt::FramelessWindowHint);
+    setWindowFlags(Qt::FramelessWindowHint|Qt::WindowStaysOnTopHint);
     setAttribute(Qt::WA_TranslucentBackground);
     QWSServer::setBackground(QColor(0,0,0,0));
     ui->cancel->setVisible(btshow);
     ui->sure->setVisible(btshow);
 
     // 子窗口
-    switch (type) {
-    case SF_485ERR:
-        _uiOpt = new SF485Error;
-        break;
-    case SF_INPUTLINE:
-        _uiOpt = new SFInputLine;
-        break;
-    case SF_LINETEXT:
-    default:
-        _uiOpt = new SFLineText;
-        break;
-    }
+    _uiOpt = NewInterWin(type);
 
     _uiOpt->bindUi(ui->widget);
     _type = type;
+    // 设置控件位置
     ui->frame->setGeometry(rect);
     showMaximized();
 }
 
 SpringFrame::~SpringFrame()
 {
+    printf("销毁弹框\n");
     delete _uiOpt;
     delete ui;
 }
@@ -81,45 +72,145 @@ void SpringFrame::showTimeOut(TIMEOUT_EV e, int sec)
     _event = e;
     _count = sec;
     QTimer::singleShot(1000, this, SLOT(countTimeOut()));
+    if (SFEV_DIALOG_REJECT==e)
+        exec();
+    else
+        show();
+}
+
+void DelLayoutItem(QLayoutItem* layoutItem)
+{
+    if (layoutItem->layout())
+    {
+        printf("是一个layout\n");
+        QLayoutItem* layout = NULL;
+        while((layout=((QLayout*)layoutItem)->takeAt(0)))
+            DelLayoutItem(layout);
+    }
+
+    printf("删除一个布局控件\n");
+    delete layoutItem;
+}
+
+// 返回值为true说明有切换控件操作
+bool SpringFrame::switchType(SF_TYPE type, const QRect& rect)
+{
+
+    // 设置控件位置
+    ui->frame->setGeometry(rect);
+
+    if (_type==type)
+        return false;
+
+    // 考虑删除弹簧
+    // 删除所有的子控件
+#if 0
+    const QList<QObject*>& items = ui->widget->findChildren<QObject*>();
+    printf("控件数量 %d\n", items.count());
+    for (QObject *item : items) {
+        qDebug() << item->objectName();
+        delete item;
+    }
+#else
+    const QList<QWidget*>& items = ui->widget->findChildren<QWidget*>();
+    printf("控件数量 %d\n", items.count());
+    for (QWidget *item : items) {
+        qDebug() << item->objectName();
+        delete item;
+    }
+
+    DelLayoutItem(ui->widget->layout());
+#if 0
+    QLayoutItem* layout = NULL;
+    while ((layout=ui->widget->layout()->takeAt(0)))
+    {
+        printf("xxxx %s\n", "删除一个布局控件");
+        delete layout;
+    }
+    delete (ui->widget->layout());
+#endif
+#endif
+
+    if (NULL!=_uiOpt)
+    {
+        delete _uiOpt;
+        _uiOpt = NULL;
+    }
+
+    _uiOpt = NewInterWin(type);
+    _uiOpt->bindUi(ui->widget);
+    _type = type;
+
+    return true;
+}
+
+InterUi *SpringFrame::NewInterWin(SF_TYPE type)
+{
+    InterUi* interUI = NULL;
+    // 子窗口
+    switch (type) {
+    case SF_485ERR:
+        interUI = new SF485Error;
+        break;
+    case SF_INPUTLINE:
+        interUI = new SFInputLine;
+        break;
+    case SF_LINEBOX:
+        interUI = new SFLineBox;
+        break;
+    case SF_LINETEXT:
+    default:
+        interUI = new SFLineText;
+        break;
+    }
+
+    return interUI;
 }
 
 void SpringFrame::on_sure_clicked()
 {
     // 控件销毁，执行子控件的获取数据函数
+    if (NULL==_uiOpt)
+        return ;
     _uiOpt->returnInput();
     accept();
-    deleteLater();
+    //deleteLater();
 }
 
 void SpringFrame::on_cancel_clicked()
 {
     // 控件销毁/隐藏，根据继承类重写决定
     reject();
-    deleteLater();
+    //deleteLater();
 }
 
 void SpringFrame::countTimeOut()
 {
+#if 1
     switch (_event)
     {
     case SFEV_REBOOT:
+        // 更新数字
+        _uiOpt->updateCount(_count);
         break;
     case SFEV_DIALOG_EXIT:
     case SFEV_DIALOG_HIDE:
     default:
         break;
     }
+#endif
 
     if (0==_count)
     {
         // 时间到
         switch (_event) {
         case SFEV_REBOOT:
-
+            printf("弹框启动重启\n");
             break;
         case SFEV_DIALOG_EXIT:
             this->deleteLater();
             break;
+        case SFEV_DIALOG_REJECT:
         case SFEV_DIALOG_HIDE:
             // 隐藏
             this->hide();
--- SpringFrame/springframe.h
+++ SpringFrame/springframe.h
@@ -1,9 +1,28 @@
+/**
+ * 弹框控件
+ * 弹框行为，
+ * 1、可以直接显示，通过按键来结束显示
+ * 2、倒计时显示，自定义显示时间（持续显示时间长度）
+ *  1、倒计时后有3中行为，自我销毁，隐藏，重启设备
+ * 弹框内部自定义显示子控件
+ * 1、显示一行文字
+ * 2、显示一个输入框，带lable的那种
+ * 3、显示485出错提示（自定义，不通用）
+ * 弹框主体包括一个控件主体和2个按键，可以对其设置样式
+ * 主要，窗体设置背景色或背景图片
+ * 按键设置背景色或背景图片，文字大小及颜色
+*/
 #ifndef SPRINGFRAME_H
 #define SPRINGFRAME_H
 #include "interUi/interui.h"
 #include "interUi/sfinputline.h"
 #include "interUi/sflinetext.h"
 #include "interUi/sf485error.h"
+#include "interUi/sflinebox.h"
+#include "ui_inputline.h"
+#include "ui_springframe.h"
+#include "ui_messagelinebox.h"
+#include "ui_messagelinetext.h"
 #include <QDialog>
 #include <QSize>
 
@@ -11,6 +30,7 @@
 typedef enum {
     SF_LINETEXT = 0,     // 显示一行文字
     SF_INPUTLINE,       // 显示一个输入框
+    SF_LINEBOX,         // 显示一个提示框
     SF_485ERR           // 显示底部2行文字显示，顶部一个图片显示
 } SF_TYPE;
 
@@ -19,20 +39,22 @@ typedef enum {
     SFEV_REBOOT,
     SFEV_DIALOG_EXIT,
     SFEV_DIALOG_HIDE,
+    SFEV_DIALOG_REJECT,
 } TIMEOUT_EV;
 
 namespace Ui {
 class SpringFrame;
 class MessageLineText;
 class InputLine;
+class MessageLineBox;
 }
 
 class SpringFrame : public QDialog
 {
     Q_OBJECT
-
 public:
-    explicit SpringFrame(QWidget *parent = 0, SF_TYPE type=SF_LINETEXT, const QRect& rect=QRect(0,0,100,100), bool btshow=false);
+    explicit SpringFrame(QWidget *parent = 0, SF_TYPE type=SF_LINETEXT,
+                         const QRect& rect=QRect(0,0,100,100), bool btshow=false);
     ~SpringFrame();
     // 设置frame的样式
     void setFrameStyle(const QString& style);
@@ -44,14 +66,21 @@ public:
     InterUi* getUiOpt();
     // 计时显示
     void showTimeOut(TIMEOUT_EV e=SFEV_DIALOG_HIDE, int sec=3);
+    // 显示
     // 切换类型
+    bool switchType(SF_TYPE type, const QRect &rect);
+
+protected:
+    // 实例化子窗口
+    InterUi* NewInterWin(SF_TYPE type);
+
 
 private slots:
     void on_sure_clicked();
     virtual void on_cancel_clicked();
     void countTimeOut();
 
-private:
+protected:
     Ui::SpringFrame *ui;
     SF_TYPE _type;
     InterUi* _uiOpt;
