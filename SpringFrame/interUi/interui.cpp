bd4f1c26b1c926880ccdd9dfa60eb33a3111eace
zhuhuihui <1340044380@qq.com>
2022-03-04 10:41:03 +0800

    子控件基类添加了一个更新lable的接口

--- SpringFrame/interUi/interui.cpp
+++ SpringFrame/interUi/interui.cpp
@@ -13,5 +13,11 @@ InterUi::~InterUi()
 
 void InterUi::returnInput()
 {
+    // 没有返回值操作的子控件什么都不干
+}
 
+void InterUi::updateCount(int num)
+{
+    Q_UNUSED(num)
+    // 没有更新计数需求的子控件什么都不干
 }
--- SpringFrame/interUi/interui.h
+++ SpringFrame/interUi/interui.h
@@ -1,3 +1,7 @@
+/**
+ * 子控件基类，定义子控件的通用接口
+ * 子控件不是控件实例，只在主控件中添加组件
+*/
 #ifndef IFACEINTERUI_H
 #define IFACEINTERUI_H
 #include <QString>
@@ -9,6 +13,7 @@ public:
     InterUi();
     virtual ~InterUi();
     virtual void returnInput();
+    virtual void updateCount(int num);
     virtual void bindUi(QWidget* w) = 0;
 };
 
