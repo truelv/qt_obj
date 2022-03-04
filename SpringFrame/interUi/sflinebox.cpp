c9e17329ffd43af9bc156da6f220ca45d4628f19
zhuhuihui <1340044380@qq.com>
2022-03-04 10:29:31 +0800

    弹框组件，添加单行提示弹框子控件

---
+++ SpringFrame/interUi/sflinebox.cpp
@@ -0,0 +1,7 @@
+#include "sflinebox.h"
+
+SFLineBox::SFLineBox()
+{
+
+}
+
---
+++ SpringFrame/interUi/sflinebox.h
@@ -0,0 +1,11 @@
+#ifndef SFLINEBOX_H
+#define SFLINEBOX_H
+
+
+class SFLineBox : public InterUi
+{
+public:
+    SFLineBox();
+};
+
+#endif // SFLINEBOX_H
