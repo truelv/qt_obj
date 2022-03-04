70c22eb9642730a5ba79821c7796ac47ddbb6acb
zhuhuihui <1340044380@qq.com>
2022-03-04 10:39:41 +0800

    删除消息提示行子控件的样式灯设置接口，改为在自定义源代码里面获取子控件设置

--- SpringFrame/interUi/sflinetext.cpp
+++ SpringFrame/interUi/sflinetext.cpp
@@ -12,23 +12,9 @@ SFLineText::~SFLineText()
     delete ui;
 }
 
-void SFLineText::setLineTextStyle(const QString &style)
-{
-    ui->label->setStyleSheet(style);
-}
-
-void SFLineText::setLineTextAlignment(Qt::Alignment a)
-{
-    ui->label->setAlignment(a);
-}
-
 void SFLineText::bindUi(QWidget *w)
 {
     ui->setupUi(w);
 }
 
-void SFLineText::setShowText(const QString &text)
-{
-    ui->label->setText(text);
-}
 
--- SpringFrame/interUi/sflinetext.h
+++ SpringFrame/interUi/sflinetext.h
@@ -9,10 +9,7 @@ class SFLineText : public InterUi
 public:
     SFLineText();
     ~SFLineText();
-    void setLineTextStyle(const QString& style);
-    void setLineTextAlignment(Qt::Alignment a);
     void bindUi(QWidget* w);
-    void setShowText(const QString& text);
 
     Ui::MessageLineText* ui;
 };
