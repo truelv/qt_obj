95b0b8ba9712607dded0a301b832fafc50cb9571
cp <18768590968@163.com>
2024-09-18 16:41:38 +0800

    添加同步执行指令接口

--- WinTools/servers/telnet/telnetsv.cpp
+++ WinTools/servers/telnet/telnetsv.cpp
@@ -18,6 +18,11 @@ TelnetSV::TelnetSV(const QString& ip):
     _tel->connectToHost(ip);
     //_tel->sendData("root");
 
+    // 清空sem
+    int n = _sem.available();
+    _sem.acquire(n);
+    _cmdWait.clear();
+    _recvMsg = nullptr;
 }
 
 TelnetSV::~TelnetSV()
@@ -42,6 +47,57 @@ int TelnetSV::ExeCommond(const QString &cmd)
 {
     qDebug() << "exe " << cmd;
     _tel->sendData(cmd+QString("\r\n"));
+    return 0;
+}
+
+#include <QCoreApplication>
+/**
+ * @brief TelnetSV::ExeCommondWait
+ * @param cmd   执行命令
+ * @param rely  返回内容
+ * @param ms    超时时间
+ * @return
+ */
+int TelnetSV::ExeCommondWait(const QString &cmd, QString* rely, int ms)
+{
+    qDebug() << "exe " << cmd;
+    if (nullptr==rely || ms<50)
+    {
+        qDebug() << "rely is null";
+        return -1;
+    }
+
+    if (!_cmdWait.isEmpty())
+    {
+        qDebug() << "cmd not null";
+        return -2;
+    }
+
+    _cmdWait = cmd;
+    _recvMsg = rely;
+    _tel->sendData(cmd+QString("\r\n"));
+    QCoreApplication::processEvents();
+
+#if 1
+    // wait
+    int n = ms/50;
+    while (n>0 && !_sem.tryAcquire(1,50)) {
+        n--;
+        QCoreApplication::processEvents();
+    }
+
+    if (n<=0)
+    {
+        qDebug() << "wait rely timeout";
+        _cmdWait.clear();
+        _recvMsg = nullptr;
+        return -3;
+    }
+#endif
+
+    _cmdWait.clear();
+    _recvMsg = nullptr;
+    return 0;
 }
 
 void TelnetSV::SLotLoginreq()
@@ -70,8 +126,17 @@ void TelnetSV::SlotConnectErr(QAbstractSocket::SocketError error)
     emit Logout();
 }
 
+// 这里同步消息，如果超时后才返回，就当一般消息处理
 void TelnetSV::SlotRecvMsg(const QString &data)
 {
+    if (!_cmdWait.isEmpty())
+    {
+        qDebug() << "recv sync telnet " << data;
+        _recvMsg->append(data);
+        _sem.release(1);
+        return ;
+    }
+
     qDebug() << "recv telnet " << data;
     if (data.contains("login:"))
     {
--- WinTools/servers/telnet/telnetsv.h
+++ WinTools/servers/telnet/telnetsv.h
@@ -1,7 +1,7 @@
 #ifndef TELNETSV_H
 #define TELNETSV_H
 #include <QtTelnet>
-
+#include <QSemaphore>
 #include "../sbase.h"
 #include "modTelnet/QtTelnet"
 
@@ -15,7 +15,11 @@ public:
     int ServerStart() override;
     int ServerStop() override;
 
+    // 执行命令，异步方式
     int ExeCommond(const QString& cmd);
+    // 执行命令，同步方式
+    // 用这个接口，需要自己控制2个同步命令的时机，以免冲突
+    int ExeCommondWait(const QString& cmd, QString *rely, int ms);
 
 signals:
     void RelyMsg(const QString& msg);
@@ -36,6 +40,10 @@ private:
     // 计数登录次数
     int _loginCount;
     int _passCount;
+    // 同步接收数据的信号
+    QSemaphore _sem;
+    QString _cmdWait;
+    QString* _recvMsg;
 };
 
 #endif // TELNETSV_H
